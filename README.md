9b70d2f7d65a1bc60dd668636f82ec5e39d6d3e3
Jörn Gersdorf <joern.gersdorf@dwpbank.de>
2019-08-29 15:26:09 +0200

    Add workaround description.

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 Reproducing the bug of AVRO-2471
 
 This repository contains a JUnit test to reproduce https://issues.apache.org/jira/browse/AVRO-2471.
+
+## Workaround
+
+The workaround for the bug is to manually register a the conversion:
+
+```java
+AvdlUnion1Record record = ...;
+record.getSpecificData().addLogicalTypeConversion(TimestampMicrosConversion());
+```
--- src/test/java/de/j0xaf/test/avro/jira2471/TestAvroJira2471.java
+++ src/test/java/de/j0xaf/test/avro/jira2471/TestAvroJira2471.java
@@ -7,17 +7,35 @@ import de.j0xaf.test.avro.jira2471.avdl.AvdlUnion1Record;
 import io.confluent.kafka.schemaregistry.client.MockSchemaRegistryClient;
 import io.confluent.kafka.serializers.KafkaAvroSerializer;
 import java.time.Instant;
+import java.util.Arrays;
 import java.util.Map;
+import org.apache.avro.Conversion;
+import org.apache.avro.data.TimeConversions.TimestampMicrosConversion;
 import org.junit.Test;
 
 public class TestAvroJira2471 {
 
-  // this is failing due to AVRO-2471
+  /**
+   * this is failing due to AVRO-2471
+   */
   @Test
   public void testAvdlUnion() {
+    testAvdlBug();
+  }
+
+  /**
+   * A workaround is to add manually the {@link TimestampMicrosConversion}.
+   */
+  @Test
+  public void testAvdlUnionWorkaround() {
+    testAvdlBug(new TimestampMicrosConversion());
+  }
+
+  private void testAvdlBug(Conversion<?>... additionalConversions) {
     final var union1Record = AvdlUnion1Record.newBuilder()
         .setNullableInstant(Instant.now())
         .build();
+    Arrays.stream(additionalConversions).forEach(c -> union1Record.getSpecificData().addLogicalTypeConversion(c));
     try (KafkaAvroSerializer s = new KafkaAvroSerializer(new MockSchemaRegistryClient(),
         Map.of(
             AUTO_REGISTER_SCHEMAS, true,
